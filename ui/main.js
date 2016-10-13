bb98d564f38eb12cb0706747ec0c156a6b5f97a2
Gowtham Krvz <gowtham.krvzkv@gmail.com>
2016-10-13 14:12:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('text');
 element.innerHTML = 'hi this is gowtham_krvz';
 console.log();
-var myAge = console.log(prompt('What\s your name?'));
+var myAge = console.log(prompt('What\s your age?'));
 if(myAge === 22)
 {
     document.write("As i suspected you are three years older than me");
