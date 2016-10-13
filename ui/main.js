f7b52d7e33d8bd65f734569b7246af138dc51adb
Gowtham Krvz <gowtham.krvzkv@gmail.com>
2016-10-13 14:57:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,8 @@ element.innerHTML = 'hi this is gowtham_krvz and iam your web app';
 
 var element = document.getElementById('steve');
     function moveRight() {
-    marginLeft = marginLeft + 10;    
+    marginLeft = marginLeft + 10;   
+    element.style.marginLeft = marginLeft + 'px';
     }
     
     element.onclick = function() {
