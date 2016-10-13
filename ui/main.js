5c8c4051a3b585bca39effbc2d14d5af8b879de2
Gowtham Krvz <gowtham.krvzkv@gmail.com>
2016-10-13 14:59:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,13 +3,13 @@ console.log('Loaded!');
 var element = document.getElementById('text');
 element.innerHTML = 'hi this is gowtham_krvz and iam your web app';
 
-var element = document.getElementById('steve');
+var img = document.getElementById('steve');
     function moveRight() {
     marginLeft = marginLeft + 10;   
-    element.style.marginLeft = marginLeft + 'px';
+    img.style.marginLeft = marginLeft + 'px';
     }
     
-    element.onclick = function() {
+    img.onclick = function() {
     var interval = setInterval(moveRight, 100);
     };
 
