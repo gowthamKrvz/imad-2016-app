936262a727e075f08634d3d0dbb665a505d56f7c
Gowtham Krvz <gowtham.krvzkv@gmail.com>
2016-10-16 07:30:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,9 @@
- //Counter code
- var button = document.getElementById('counter');
- button.onclick = function () {
-   
-   
-   
-   
-   counter = counter + 1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
- };
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+element.onclick = function() {
+  counter = counter +1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+    
+};
